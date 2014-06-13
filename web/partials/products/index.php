7ef94ffea9b09db7821af796a0957fa08e99b114
Filriya <administrator@www9434uo.sakura.ne.jp>
2014-06-13 19:41:48 +0900

    product文言修正途中まで

--- web/partials/products/index.php
+++ web/partials/products/index.php
@@ -16,17 +16,18 @@
           </h2>
           <div class="text">
             <p>
-              AiファイルやPSDファイルを正確に画像ファイルに変換するネイティブエンジンを利用したコンバータ。
+              美しいUI・アニメーションで現在の体内アルコール濃度や酔いが覚めるまでの時間を<br/>
+              リアルタイムで計算して表示する無料iPhoneアプリ。
             </p>
             <p>
-              D&DでコンバートするカジュアルなインターフェースからWebAPIまで利用可能。
+              二日酔いのおそれや摂取カロリーだって知らせてくれます。以上の情報をシェアする機能も搭載。
             </p>
             <a href="/alcCalc" class="link-button able">
               <span class="button-content">
                 <span class="button-image">
                   <img src="/images/share/icon_view.png"/>
                 </span>
-                <span class="button-text">www.aipsconv.com</span>
+                <span class="button-text">alcCalc</span>
               </span>
             </a>
             <!--
@@ -37,9 +38,9 @@
             -->
             <br/>
             <p class="cooperator">
-              Director,Artwork HiraiKeiichiro<br/>
-              Engineer FurutaniTakuma,Uhei<br/>
-              Yamamori,TsuyoshiYasuda
+              Director,Artwork KeiichiroHirai<br/>
+              Engineer TakumaFurutani<br/>
+              Assistant AtsushiTomofuji
             </p>
           </div>
         </div>
@@ -149,7 +150,7 @@
   <div class="promotion">
     <header class="panel banner">
       <p class="title">
-        RUBBER NOVELTY<br/>
+        NOVELTY SUPPLY<br />
         HOLDA
       </p>
     </header>
@@ -158,24 +159,21 @@
         <div class="content">
           <div class="thumbnail"><img src="/images/products/img_sample_aips.png"/></div>
           <h2 class="title">
-            RUBBER NOVELTY<br/>
+            NOVELTY SUPPLY<br />
             HOLDA
           </h2>
           <div class="text">
             <p>
-              AiファイルやPSDファイルを正確に画像ファイルに変換するネイティブエンジンを利用したコンバータ。
+              オリジナルデザインのラバーストラップやキーホルダーなどを製作できるサービス。
             </p>
-            <p>
-              D&DでコンバートするカジュアルなインターフェースからWebAPIまで利用可能。
-            </p>
-            <a class="link-button able">
-              <span class="button-content">
-                <span class="button-image">
-                  <img src="/images/share/icon_view.png"/>
+            <div class="link-button disable">
+              <div class="content">
+                <span class="icon">
+                  <img src="/images/share/icon_view_disable.png"/>
                 </span>
-                <span class="button-text">www.aipsconv.com</span>
-              </span>
-            </a>
+                <span class="disable-text">COMING 2014</span>
+              </div>
+            </div>
             <!--
             <span class="twitter">
               <span class="image"><img src="/images/share/icon_twitter.png"/></span>
@@ -184,9 +182,9 @@
             -->
             <br/>
             <p class="cooperator">
-              Director,Artwork HiraiKeiichiro<br/>
-              Engineer FurutaniTakuma,Uhei<br/>
-              Yamamori,TsuyoshiYasuda
+              Director,Design KeiichiroHirai<br/>
+              Frontend YuheiYamamori<br/>
+              Backend TsuyoshiYasuda TakumaFurutani
             </p>
           </div>
         </div>
@@ -212,9 +210,7 @@
           </h2>
           <div class="text">
             <p>
-              AIファイル、PSDファイルなどからオリジナルデザインの缶バッジを製作できるサービス。
-            </p>
-            <p>
+              AIファイル、PSDファイルなどからオリジナルデザインの缶バッジを製作できるサービス。<br/>
               年間1500万個以上の製作実績を誇り、国内シェア最大級。
             </p>
             <a href="http://www.secondpress.us" class="link-button able">
@@ -233,9 +229,9 @@
             -->
             <br/>
             <p class="cooperator">
-              Director,Artwork HiraiKeiichiro<br/>
-              Engineer FurutaniTakuma,Uhei<br/>
-              Yamamori,TsuyoshiYasuda
+              Director,Design KeiichiroHirai<br />
+              Frontend YuheiYamamori<br />
+              Backend TsuyoshiYasuda,TakumaFurutani<br />
             </p>
           </div>
         </div>
@@ -247,7 +243,7 @@
   <div class="promotion">
     <header class="panel banner">
       <p class="title">
-        WEB SERVICE (NOHT LAB.)<br/>
+        VST PLUGIN<br/>
         ANTI PHASE
       </p>
     </header>
@@ -256,15 +252,13 @@
         <div class="content">
           <div class="thumbnail"><img src="/images/products/img_sample_aips.png"/></div>
           <h2 class="title">
-            WEB SERVICE (NOHT LAB.)<br/>
+            VST PLUGIN<br/>
             ANTI PHASE
           </h2>
           <div class="text">
             <p>
-              AiファイルやPSDファイルを正確に画像ファイルに変換するネイティブエンジンを利用したコンバータ。
-            </p>
-            <p>
-              D&DでコンバートするカジュアルなインターフェースからWebAPIまで利用可能。
+              入力されたソースをモノミックスにて<br/>
+              LとRの各チャンネルに正相と逆相を振分け出力するVSTプラグイン。
             </p>
             <div class="link-button disable">
               <div class="content">
@@ -282,9 +276,8 @@
             -->
             <br/>
             <p class="cooperator">
-              Director,Artwork HiraiKeiichiro<br/>
-              Engineer FurutaniTakuma,Uhei<br/>
-              Yamamori,TsuyoshiYasuda
+              Director,Designer.Engineer KeiichiroHirai<br/>
+              LAUNCHED 2008
             </p>
           </div>
         </div>
@@ -296,8 +289,8 @@
   <div class="promotion">
     <header class="panel banner">
       <p class="title">
-        VST PLUGIN (NOHT LAB.)<br/>
-        nCOMP.
+        VST PLUGIN<br/>
+        nCOMP
       </p>
     </header>
     <section class="panel description">
@@ -305,15 +298,13 @@
         <div class="content">
           <div class="thumbnail"><img src="/images/products/img_sample_aips.png"/></div>
           <h2 class="title">
-            VST PLUGIN (NOHT LAB.)<br/>
-            nCOMP.
+            VST PLUGIN<br/>
+            nCOMP
           </h2>
           <div class="text">
             <p>
-              AiファイルやPSDファイルを正確に画像ファイルに変換するネイティブエンジンを利用したコンバータ。
-            </p>
-            <p>
-              D&DでコンバートするカジュアルなインターフェースからWebAPIまで利用可能。
+              アナログの歪みをエミュレートしたシンプルなコンプレッサー／リミッター。<br/>
+              マキシマイザとしても使用可能なVSTプラグイン。
             </p>
             <div class="link-button disable">
               <div class="content">
@@ -331,9 +322,8 @@
             -->
             <br/>
             <p class="cooperator">
-              Director,Artwork HiraiKeiichiro<br/>
-              Engineer FurutaniTakuma,Uhei<br/>
-              Yamamori,TsuyoshiYasuda
+              Director,Designer.Engineer KeiichiroHirai<br/>
+              LAUNCHED 2008
             </p>
           </div>
         </div>
