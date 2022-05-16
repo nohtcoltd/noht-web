55a1496f11793afec021741bfce3063dc0e7b52c
ymmr <ymmr.test@gmail.com>
2022-05-16 17:23:14 +0900

    逆回転設定

--- components/widgets/TurnBox.vue
+++ components/widgets/TurnBox.vue
@@ -3,56 +3,66 @@ import { ref, computed } from '@vue/composition-api'
 
 const props = withDefaults(
   defineProps<{
-    pages: number
-    current: number
+    value: number
     duration: number
     perspective: number
     easing: string
     isAxisX: boolean
+    isReversed: boolean
   }>(),
   {
     duration: 500,
     perspective: 500,
     easing: 'ease-out',
     isAxisX: true,
+    isReversed: false,
   },
 )
 
-const hoge = ref<number>(props.current)
-const lastPage = ref<number>(1)
-const isIncrement = computed(() => lastPage.value < props.current)
+const last = ref(1)
+const inAnimation = ref(false)
 
-const translateValue = (el, isNext) => {
+const translation = computed(() => (el, isNext) => {
   const { clientWidth, clientHeight } = el
   const size = props.isAxisX ? clientHeight : clientWidth
-  const zPosition = size / 2
-  const flatPosition = isNext ? size / 2 : -size / 2
+  const positionZ = size / 2
+  const position2d = isNext ? -size / 2 : size / 2
 
   if (props.isAxisX) {
-    return `translate3d(0, ${flatPosition}px, ${zPosition}px)`
+    return `translate3d(0, ${position2d}px, ${positionZ}px)`
   }
 
-  return `translate3d(${-flatPosition}px, 0, ${zPosition}px)`
-}
+  return `translate3d(${-position2d}px, 0, ${positionZ}px)`
+})
 
-const rotateValue = (isNext) => {
-  const deg = isNext ? -90 : 90
+const rotation = computed(() => (isNext) => {
+  const deg = isNext ? 90 : -90
 
   if (props.isAxisX) {
     return `rotate3d(1, 0, 0, ${deg}deg)`
   }
 
   return `rotate3d(0, 1, 0, ${deg}deg)`
-}
+})
 
 const enter = async (el, done) => {
-  console.log(props.current > lastPage.value)
-  console.log(isIncrement.value)
+  inAnimation.value = true
+  el.style.position = 'relative'
+
+  el.animate(
+    {
+      opacity: [0, 1],
+    },
+    {
+      duration: props.duration * 0.3,
+    },
+  )
   await el.animate(
     {
+      zIndex: [0, 10],
       transform: [
-        `${rotateValue(!isIncrement.value)} ${translateValue(el, !isIncrement.value)}`,
-        `rotate3d(${props.isAxisX ? '1, 0' : '0, 1'}, 0, 0) translate3d(0, 0, 0px)`,
+        `${rotation.value(!props.isReversed)} ${translation.value(el, !props.isReversed)}`,
+        `rotate3d(${props.isAxisX ? '1, 0' : '0, 1'}, 0, 0) translate3d(0, 0, 0)`,
       ],
     },
     {
@@ -61,7 +71,8 @@ const enter = async (el, done) => {
     },
   ).finished
 
-  lastPage.value = props.current
+  inAnimation.value = false
+  last.value = props.value
 
   done()
 }
@@ -72,9 +83,10 @@ const leave = async (el, done) => {
 
   await el.animate(
     {
+      zIndex: [10, 0],
       transform: [
-        `rotate3d(1, 0, 0, 0) translate3d(0, 0, 0px)`,
-        `${rotateValue(isIncrement.value)} ${translateValue(el, isIncrement.value)}`,
+        `rotate3d(1, 0, 0, 0) translate3d(0, 0, 0)`,
+        `${rotation.value(props.isReversed)} ${translation.value(el, props.isReversed)}`,
       ],
     },
     {
@@ -84,6 +96,14 @@ const leave = async (el, done) => {
   ).finished
   done()
 }
+
+const cancelEnter = (el, done) => {
+  console.log('cancelEnter', el, done)
+}
+
+const cancelLeave = (el, done) => {
+  console.log('cancelLeave', el, done)
+}
 </script>
 
 <template>
@@ -92,12 +112,18 @@ const leave = async (el, done) => {
     :style="{
       perspective: `${perspective}px`,
     }"
-    @click="hoge = hoge + 1"
   >
-    <transition :css="false" @enter="enter" @leave="leave">
-      <div :key="`page-${current}`">
-        <slot :name="`page-${current}`" />
+    <transition
+      :css="false"
+      @enter="enter"
+      @leave="leave"
+      @enter-cancelled="cancelEnter"
+      @leave-cancelled="cancelLeave"
+    >
+      <div :key="`face-${value}`">
+        <slot :name="`face-${value}`" />
       </div>
     </transition>
+    <div v-if="inAnimation" class="absolute top-0 left-0 z-10 h-full w-full" />
   </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -10,52 +10,55 @@ const changePage = (page) => (current.value = page)
 <template>
   <article>
     <div class="py-32 fl-col-nowrap fl-start-stretch-stretch">
-      <turn-box :pages="2" :current="current" class="mx-auto w-[300px]">
-        <template #page-1>
+      <turn-box v-model="current" class="mx-auto w-[300px]">
+        <template #face-1>
           <div class="p-1.5">
             <div @click="changePage(2)" class="select-none rounded-md bg-[#888] p-[20px]">ほげ</div>
           </div>
         </template>
-        <template #page-2>
+        <template #face-2>
           <div class="p-1.5">
             <div @click="changePage(1)" class="select-none rounded-md bg-black p-[20px] text-white">ふが</div>
           </div>
         </template>
       </turn-box>
-      <turn-box :pages="2" :current="current" class="mx-auto w-[300px]" :is-axis-x="false">
-        <template #page-1>
+      <turn-box v-model="current" class="mx-auto w-[300px]" :is-axis-x="false" :is-reversed="current === 2">
+        <template #face-1>
           <div class="p-1.5">
             <div @click="changePage(2)" class="select-none rounded-md bg-[#888] p-[20px]">ほげ</div>
           </div>
         </template>
-        <template #page-2>
+        <template #face-2>
           <div class="p-1.5">
             <div @click="changePage(1)" class="select-none rounded-md bg-black p-[20px] text-white">ふが</div>
           </div>
         </template>
       </turn-box>
-      <turn-box :pages="5" :current="tst" class="mx-auto w-[300px]" :is-axis-x="true">
-        <template #page-1>
+      <turn-box v-model="tst" class="mx-auto w-[300px]" :is-axis-x="true">
+        <template #face-1>
           <div class="p-1.5">
             <div @click="tst = tst + 1" class="select-none rounded-md bg-[#888] p-[20px]">ほげ1</div>
           </div>
         </template>
-        <template #page-2>
+        <template #face-2>
           <div class="p-1.5">
-            <div @click="tst = tst + 1" class="select-none rounded-md bg-[#888] p-[20px]">ほげ2</div>
+            <div @click="tst = tst + 1" class="select-none rounded-md bg-[#888] p-[20px]">
+              ほげ2<br />
+              ほげほげ
+            </div>
           </div>
         </template>
-        <template #page-3>
+        <template #face-3>
           <div class="p-1.5">
             <div @click="tst = tst + 1" class="select-none rounded-md bg-[#888] p-[20px]">ほげ3</div>
           </div>
         </template>
-        <template #page-4>
+        <template #face-4>
           <div class="p-1.5">
             <div @click="tst = tst + 1" class="select-none rounded-md bg-[#888] p-[20px]">ほげ4</div>
           </div>
         </template>
-        <template #page-5>
+        <template #face-5>
           <div class="p-1.5">
             <div @click="tst = 1" class="select-none rounded-md bg-[#888] p-[20px]">ほげ5</div>
           </div>
