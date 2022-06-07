ce74e949ca1facfdaeff9af915b552aa1848475c
ymmr <ymmr.test@gmail.com>
2022-06-07 17:37:54 +0900

    contactページ

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-import { reactive, ref } from '@nuxtjs/composition-api'
+import { reactive, inject, ref, ComponentInstance, useRoute } from '#app'
+import MyInput from '~/components/elements/MyInput.vue'
+import ErrorMessage from '~/components/widgets/ErrorMessage.vue'
+import { addCompleteForwardRotationHandle } from '~/composables/useTurnPage'
 
+const route = useRoute()
+const $observer = ref<ComponentInstance>(null)
 const fields = reactive({
   name: '',
   companyName: '',
   mail: '',
-  mailConfirm: '',
   content: '',
 })
 
@@ -15,7 +19,6 @@ const submit = async () => {
   formData.append('name', fields.name)
   formData.append('company-name', fields.companyName)
   formData.append('mail', fields.mail)
-  formData.append('mail-confirm', fields.mailConfirm)
   formData.append('content', fields.content)
   formData.append('form-name', 'contact')
 
@@ -23,9 +26,25 @@ const submit = async () => {
     method: 'POST',
     body: formData,
   })
-
-  console.log(data)
 }
+
+const reset = () => {
+  $observer.value.reset()
+  fields.name = ''
+  fields.companyName = ''
+  fields.mail = ''
+  fields.content = ''
+}
+
+const addHandle = inject(addCompleteForwardRotationHandle)
+
+addHandle(() => {
+  if (route.name === 'contact') {
+    return
+  }
+
+  reset()
+})
 </script>
 
 <template>
@@ -33,15 +52,37 @@ const submit = async () => {
     <form netlify name="hoge">
       <input name="form-name" value="hoge" />
   -->
-  <form name="contact" netlify class="fl-col-nowrap fl-start-center-center">
-    <input type="hidden" name="form-name" value="contact" />
-    {{ fields }}
-    <input v-model="fields.name" placeholder="name" name="name" class="mt-3" />
-    <input v-model="fields.companyName" placeholder="company-name" name="company-name" class="mt-3" />
-    <input v-model="fields.mail" placeholder="mail" name="mail" class="mt-3" />
-    <input v-model="fields.mailConfirm" placeholder="mail-confirm" name="mail-confirm" class="mt-3" />
-    <textarea v-model="fields.content" placeholder="content" name="content" class="mt-3" />
+  <validation-observer ref="$observer" v-slot="{ handleSubmit }" slim>
+    <form name="contact" netlify class="w-full text-[16px] fl-col-nowrap fl-start-center-center mb:text-[14px]">
+      <input type="hidden" name="form-name" value="contact" />
+      <div class="w-full max-w-[350px]">
+        <validation-provider mode="passive" rules="required" v-slot="{ errors }" slim>
+          <MyInput v-model="fields.name" :error="errors[0]" placeholder="NAME" name="name" class="mt-[1.2em]" />
+          <ErrorMessage :error="errors[0]" />
+        </validation-provider>
+        <MyInput v-model="fields.companyName" placeholder="COMPANY" name="company-name" class="mt-[1.2em]" />
+        <validation-provider mode="passive" rules="required|email" v-slot="{ errors }" slim>
+          <MyInput v-model="fields.mail" :error="errors[0]" placeholder="MAIL" name="mail" class="mt-[1.2em]" />
+          <ErrorMessage :error="errors[0]" />
+        </validation-provider>
+      </div>
+      <validation-provider class="block w-full" mode="passive" rules="required" v-slot="{ errors }" slim>
+        <textarea
+          v-model="fields.content"
+          :error="errors[0]"
+          name="content"
+          class="mt-[3em] aspect-[5/2] w-full resize-none rounded-[15px] border border-solid border-transparent bg-[#f7f7f7] p-[1em] font-semibold outline-none placeholder:text-[length:inherit] mb:mt-[2em]"
+          :class="errors.length !== 0 ? 'border-[#ec3232]' : ''"
+        />
+        <ErrorMessage :error="errors[0]" />
+      </validation-provider>
 
-    <div type="submit" @click="submit">OK</div>
-  </form>
+      <div
+        @click="handleSubmit(submit)"
+        class="mt-[4em] cursor-pointer select-none rounded-[.5em] bg-[#111] py-[.5em] px-[4em] text-[min(120%,25px)] font-semibold tracking-[.2em] text-white font-poppins my-hover:opacity-80"
+      >
+        CONFIRM
+      </div>
+    </form>
+  </validation-observer>
 </template>
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+import ContactForm from '~/components/ContactForm.vue'
+</script>
 <template>
-<article class="h-screen bg-slate-300">
-  <contact-form />
-</article>
+  <article
+    class="bg-white px-[3em] text-[length:clamp(20px,2.3vw,28px)] fl-col-nowrap fl-start-center-center tablet:px-0"
+  >
+    <div class="text-[150%] font-semibold tracking-[.2em] font-poppins mb:text-[20px]">CONTACT</div>
+    <contact-form class="mt-[2em] max-w-[800px] mb:mt-[1.5em] mb:max-w-[350px]" />
+  </article>
 </template>
