d29d69e805f4820c0b9cfeac792af38473d844b4
ymmr <ymmr.test@gmail.com>
2022-06-09 00:16:10 +0900

    [hotfix] メタディスクリプション

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,12 @@ export default defineNuxtConfig({
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width,initial-scale=1.0,maximum-scale=1.0' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          '株式会社ノート（NOHT CO.,LTD.）はウェブサイトやサービスの企画・運営、システムやアプリケーションの開発を行っています。',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     // link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
