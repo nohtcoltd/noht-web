386f631f8e91b9e52a1b9cd948d835f0f0606dc1
ymmr <ymmr.test@gmail.com>
2022-06-08 17:37:21 +0900

    thanksのアニメション

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { ref, inject } from '#app'
+import { ref, inject, useRoute } from '#app'
 import ContactForm from '~/components/ContactForm.vue'
 import { addCompleteForwardRotationHandle } from '~/composables/useTurnPage'
-
+import CloseButton from '~/components/widgets/CloseButton.vue'
+const route = useRoute()
 const isThanksShown = ref(false)
 
 const showThanks = () => (isThanksShown.value = true)
@@ -11,11 +12,43 @@ const hideThanks = () => (isThanksShown.value = false)
 const addHandle = inject(addCompleteForwardRotationHandle)
 
 addHandle(hideThanks)
+
+const enter = ($el: HTMLElement, done: () => void) => {
+  if (route.name !== 'contact') {
+    done()
+    return
+  }
+
+  $el.animate(
+    {
+      opacity: [0, 1],
+    },
+    {
+      duration: 200,
+    },
+  ).finished
+
+  done()
+}
 </script>
 <template>
-  <article class="bg-white px-[3em] text-[length:clamp(20px,2.3vw,28px)] tablet:px-0">
-    <div v-if="isThanksShown">THANKS</div>
-    <div v-else class="fl-col-nowrap fl-start-center-center">
+  <article class="relative bg-white px-[3em] text-[length:clamp(25px,2.3vw,28px)] tablet:px-0">
+    <transition appear :css="false" @enter="enter">
+      <div
+        v-if="isThanksShown"
+        key="thanks"
+        class="absolute top-0 left-0 right-0 m-auto h-[70vh] fl-col-nowrap fl-center-center-center mb:h-[50vh]"
+      >
+        <div class="text-[180%] font-semibold tracking-[.2em] font-poppins mb:text-[25px]">THANK YOU!</div>
+        <div class="mt-[80px] pc:w-full pc:max-w-[80px]">
+          <n-link :to="{ name: 'index' }">
+            <close-button class="pc:min-w-full" />
+          </n-link>
+        </div>
+      </div>
+    </transition>
+
+    <div v-show="!isThanksShown" class="fl-col-nowrap fl-center-center-center">
       <div class="text-[150%] font-semibold tracking-[.2em] font-poppins mb:text-[20px]">CONTACT</div>
       <contact-form class="mt-[2em] max-w-[800px] mb:mt-[1.5em] mb:max-w-[350px]" @sent="showThanks" />
     </div>
