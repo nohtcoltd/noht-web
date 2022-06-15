a51553b0403834345bb1d7b23b64af96380e14f8
ymmr <ymmr.test@gmail.com>
2022-06-15 20:31:10 +0900

    [hotfix] スタッフ名をclient-onlyに

--- components/pages/index/Staff.vue
+++ components/pages/index/Staff.vue
@@ -9,14 +9,16 @@ defineProps<{
   <div
     class="text-center font-semibold leading-[1.3] tracking-[.1em] fl-col-nowrap fl-start-center-center font-poppins"
   >
-    <div class="tracking-[.2em]">{{ name }}</div>
-    <div class="mt-[.5em] text-[60%] fl-col-nowrap fl-start-center-center word-break mb:text-[80%]">
-      <template v-if="Array.isArray(jobTitle)">
-        <span v-for="(str, index) in jobTitle" :key="`key-${index}`"> {{ str }}</span>
-      </template>
-      <template v-else>
-        {{ jobTitle }}
-      </template>
-    </div>
+    <client-only>
+      <div class="tracking-[.2em]">{{ name }}</div>
+      <div class="mt-[.5em] text-[60%] fl-col-nowrap fl-start-center-center word-break mb:text-[80%]">
+        <template v-if="Array.isArray(jobTitle)">
+          <span v-for="(str, index) in jobTitle" :key="`key-${index}`"> {{ str }}</span>
+        </template>
+        <template v-else>
+          {{ jobTitle }}
+        </template>
+      </div>
+    </client-only>
   </div>
 </template>
