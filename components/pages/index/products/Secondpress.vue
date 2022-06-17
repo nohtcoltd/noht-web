6a45ab1f90120a8164faade6ff384494d91a920e
ymmr <ymmr.test@gmail.com>
2022-06-17 21:38:40 +0900

    [hotfix] スタッフ表記の間違い修正

--- components/pages/index/products/Secondpress.vue
+++ components/pages/index/products/Secondpress.vue
@@ -49,7 +49,7 @@ import Staff from '~/components/pages/index/Staff.vue'
       >
         <Staff name="KEIICHIRO HIRAI" job-title="PLAN/DIRECTOR/DESIGN" />
         <div class="col-span-5 mb:col-span-2" />
-        <Staff name="FILRIYA" job-title="TEC LEAD" />
+        <Staff name="FILRIYA" job-title="TECH LEAD" />
         <Staff name="YOUGO NISHIOKA " job-title="BACKEND LEAD" />
         <Staff name="YMMR" job-title="FRONTEND" />
         <Staff name="AZARASHI" job-title="IMAGE CALC." />
