473ce98e24594ad8e41af2523c282d3284c765e3
ymmr <ymmr.test@gmail.com>
2022-06-20 22:24:31 +0900

    vee-validateのアップグレード

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -3,20 +3,34 @@ import MyInput from '~/components/elements/MyInput.vue'
 import ErrorMessage from '~/components/widgets/ErrorMessage.vue'
 import { addCompleteForwardRotationHandle } from '~/composables/useTurnPage'
 import LoadingGear from '~/components/widgets/LoadingGear.vue'
+import { useForm } from 'vee-validate';
+import * as yup from 'yup';
 
 const { $recaptcha } = useContext()
 const hasRecaptchaError = ref(false)
 const isFailed = ref(false)
+const isSubmitted = ref(false)
 const isSubmitting = ref(false)
 
 const route = useRoute()
-const $observer = ref<ComponentInstance>(null)
-const fields = ref({
-  'form-name': 'contact',
-  name: '',
-  'company-name': '',
-  mail: '',
-  content: '',
+const formName = 'contact'
+const requiredErrorMessage = '必須項目です'
+const mailErrorMessage = '有効なメールアドレスを入力してください'
+
+const validationSchema = yup.object({
+    name: yup.string().required(requiredErrorMessage),
+    'company-name': yup.string(),
+    mail: yup.string().email(mailErrorMessage).required(requiredErrorMessage),
+    content: yup.string().required(requiredErrorMessage),
+  })
+const form = useForm({ validationSchema })
+
+const errors = computed((): { [key in keyof validationSchema]: string } => {
+  if (isSubmitted.value) {
+    return form.errors.value
+  }
+
+  return {}
 })
 
 const emits = defineEmits<{
@@ -38,6 +52,14 @@ const validateRecaptcha = async (): Promise<string> => {
 }
 
 const submit = async () => {
+  isSubmitted.value = true
+
+  const { valid } = await form.validate()
+
+  if (!valid) {
+    return
+  }
+
   hasRecaptchaError.value = false
   isFailed.value = false
   isSubmitting.value = true
@@ -52,13 +74,10 @@ const submit = async () => {
 
   const formData = new FormData()
 
-  formData.append('name', fields.value.name)
-  formData.append('company-name', fields.value['company-name'])
-  formData.append('mail', fields.value.mail)
-  formData.append('content', fields.value.content)
+  Object.keys(form.values).forEach((key) => formData.append(key, form.values[key]))
 
   // 以下netlify form用
-  formData.append('form-name', 'contact')
+  formData.append('form-name', formName)
   formData.append('g-recaptcha-response', recaptchaToken)
 
   const data = await $fetch('/', {
@@ -77,12 +96,8 @@ const submit = async () => {
 }
 
 const reset = () => {
-  fields.value.name = ''
-  fields.value['company-name'] = ''
-  fields.value.mail = ''
-  fields.value.content = ''
-  $observer.value.reset()
-  $recaptcha.reset()
+  form.resetForm()
+  // $recaptcha.reset()
 }
 
 const addHandle = inject(addCompleteForwardRotationHandle)
@@ -101,49 +116,37 @@ addHandle(() => {
     <form netlify name="hoge">
       <input name="form-name" value="hoge" />
     -->
-  <form :name="fields['form-name']" netlify data-netlify-recaptcha="true" class="w-full text-[16px] mb:text-[14px]">
-    <input v-for="(value, key) in fields" :name="key" :key="key" type="hidden" :value="value" />
+  <form :name="formName" netlify data-netlify-recaptcha="true" class="w-full text-[16px] mb:text-[14px] w-full fl-col-nowrap fl-start-center-center">
+    <input v-for="(value, key) in form.values" :name="key" :key="key" type="hidden" :value="value" />
     <client-only>
-      <validation-observer
-        ref="$observer"
-        v-slot="{ handleSubmit }"
-        class="w-full fl-col-nowrap fl-start-center-center"
-      >
-        <input type="hidden" name="form-name" :value="fields['form-name']" />
+        <input type="hidden" name="form-name" :value="formName" />
         <div class="w-full max-w-[350px]">
-          <validation-provider mode="passive" rules="required" v-slot="{ errors }" slim>
-            <MyInput v-model="fields.name" :error="errors[0]" placeholder="NAME" name="name" class="mt-[1.2em]" />
-            <ErrorMessage :error="errors[0]" />
-          </validation-provider>
-          <MyInput v-model="fields['company-name']" placeholder="COMPANY" name="company-name" class="mt-[1.2em]" />
-          <validation-provider mode="passive" rules="required|email" v-slot="{ errors }" slim>
-            <MyInput v-model="fields.mail" :error="errors[0]" placeholder="MAIL" name="mail" class="mt-[1.2em]" />
-            <ErrorMessage :error="errors[0]" />
-          </validation-provider>
+            <MyInput v-model="form.values.name" :error="errors.name" placeholder="NAME" name="name" class="mt-[1.2em]" />
+            <ErrorMessage :error="errors.name" />
+          <MyInput v-model="form.values['company-name']" placeholder="COMPANY" name="company-name" class="mt-[1.2em]" />
+          <MyInput v-model="form.values.mail" :error="errors.mail" placeholder="MAIL" name="mail" class="mt-[1.2em]" />
+          <ErrorMessage :error="errors.mail" />
         </div>
-        <validation-provider class="block w-full" mode="passive" rules="required" v-slot="{ errors }" slim>
-          <textarea
-            v-model="fields.content"
-            :error="errors[0]"
-            name="content"
-            class="mt-[3em] aspect-[5/2] w-full resize-none rounded-[15px] border border-solid border-transparent bg-[#f7f7f7] p-[1em] font-semibold outline-none placeholder:text-[length:inherit] mb:mt-[2em]"
-            :class="errors.length !== 0 ? 'border-[#ec3232]' : ''"
-          />
-          <ErrorMessage :error="errors[0]" />
-        </validation-provider>
+        <textarea
+          v-model="form.values.content"
+          :error="errors.content"
+          name="content"
+          class="mt-[3em] aspect-[5/2] w-full resize-none rounded-[15px] border border-solid border-transparent bg-[#f7f7f7] p-[1em] font-semibold outline-none placeholder:text-[length:inherit] mb:mt-[2em]"
+          :class="errors.content ? 'border-[#ec3232]' : ''"
+        />
+        <ErrorMessage :error="errors.content" />
 
         <div class="mt-[2em] min-h-[80px]">
-          <recaptcha />
+          <!-- <recaptcha /> -->
           <ErrorMessage v-if="hasRecaptchaError" error="失敗しました" />
         </div>
 
         <div
-          @click="handleSubmit(submit)"
+          @click="submit"
           class="mt-[4em] cursor-pointer select-none rounded-[.5em] bg-[#111] py-[.5em] px-[4em] text-[min(120%,25px)] font-semibold tracking-[.2em] text-white font-poppins my-hover:opacity-80"
         >
           CONFIRM
         </div>
-      </validation-observer>
     </client-only>
     <div
       v-if="isSubmitting"
